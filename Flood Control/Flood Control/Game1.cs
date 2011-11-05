2b409ca7fa96e913ed97b7340dc6841f5790d6c6
Brandon Fallout <p4r4d0x42@gmail.com>
2011-11-05 05:26:56 -0700

    Added a class for game pieces and put some code in it yay \o/

--- Flood Control/Flood Control/Game1.cs
+++ Flood Control/Flood Control/Game1.cs
@@ -19,6 +19,28 @@ namespace Flood_Control
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+
+        /// <summary>
+        /// Keyboard, gamepad and mouse support Variables
+        /// </summary>
+        KeyboardState keys;
+        GamePadState pad1;
+        MouseState mouse;
+        /*
+         * keys = Keyboard.GetState();
+         * pad1 = GamePad.GetState(PlayerIndex.One);
+         * mouse = Mouse.GetState();
+         * Drop these in your update method or when ever you need to pull data from them.
+         */
+
+        // Textures
+        Texture2D
+            playingPieces,
+            backgroundScreen,
+            titleScreen;
+
+
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -47,6 +69,11 @@ namespace Flood_Control
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            playingPieces = Content.Load<Texture2D>(@"Textures/Tile_Sheet");
+            backgroundScreen = Content.Load<Texture2D>(@"Textures/Background");
+            titleScreen = Content.Load<Texture2D>(@"Textures/TitleScreen");
+
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -66,6 +93,7 @@ namespace Flood_Control
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
---
+++ Flood Control/Flood Control/GamePiece.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+namespace Flood_Control
+{
+    class GamePiece
+    {
+
+        public static string[] PieceTypes = 
+        {
+            "Left,Right",
+            "Top,Bottom",
+            "Left,Top",
+            "Top,Right",
+            "Right,Bottom",
+            "Bottom,Left",
+            "Empty"
+        };
+
+        public const int
+            PieceHeight = 40,
+            PieceWidth = 40;
+
+
+        public const int
+            MaxPlayablePieceIndex = 5,
+            EmptyPieceIndex = 6;
+
+        public const int
+            textureOffsetX = 1,
+            textureOffsetY = 1,
+            texturePaddingX = 1,
+            texturePaddingY = 1;
+
+        private string
+            pieceType = "",
+            pieceSuffix = "";
+
+
+
+        public string PieceType
+        {
+            get { return pieceSuffix; }
+        }
+
+        public string Suffix
+        {
+            get { return pieceSuffix; }
+        }
+
+
+
+
+
+    }
+}
